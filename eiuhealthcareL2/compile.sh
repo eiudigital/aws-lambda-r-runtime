f722a13ab3e0ae4df37e72e3d383e753a350b1b3
blu3pr1n7 <asdf>
2022-01-28 11:58:15 +0000

    Added list of packages from HCGDM-109 to {compile,entrypoint}.sh scripts

--- eiuhealthcareL2/compile.sh
+++ eiuhealthcareL2/compile.sh
@@ -18,5 +18,13 @@ rm -rf ${BUILD_DIR}
 
 export R_LIBS=${BUILD_DIR}/layer/R/library
 mkdir -p ${R_LIBS}
-${R_DIR}/bin/Rscript -e 'install.packages("tidyverse", repos="http://cran.r-project.org")'
+${R_DIR}/bin/Rscript -e 'install.packages("install.load", repos="http://cran.r-project.org")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("readxl", repos="http://cran.r-project.org")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("stringr", repos="http://cran.r-project.org")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("dplyr", repos="http://cran.r-project.org")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("RCurl", repos="http://cran.r-project.org")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("XML", repos="http://cran.r-project.org")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("tidyr", repos="http://cran.r-project.org")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("purrr", repos="http://cran.r-project.org")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("tibble", repos="http://cran.r-project.org")'
 chmod -R 755 ${BUILD_DIR}/layer/
