ca770b3380034770b9395ab126080881a65e9ef3
blu3pr1n7 <asdf>
2022-01-31 11:47:34 +0000

    Updated entrypoint.sh with analysis specific packages

--- eiuhealthcareL1/entrypoint.sh
+++ eiuhealthcareL1/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -euo pipefail
 
 BUILD_DIR=/var/eiuhealthcareL1/
@@ -7,4 +6,12 @@ R_DIR=/opt/R/
 
 export R_LIBS=${BUILD_DIR}/R/library
 mkdir -p ${R_LIBS}
-${R_DIR}/bin/Rscript -e 'install.packages("tidyverse", repos="http://cran.r-project.org")'
+${R_DIR}/bin/Rscript -e 'install.packages("install.load", repos="http://cran.r-project.org")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("readxl")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("stringr")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("dplyr")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("RCurl")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("XML")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("tidyr")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("purrr")'
+${R_DIR}/bin/Rscript -e 'install.load::install_load("tibble")'
